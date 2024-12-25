330abaf02faf3e3349106946b0eecca8dda39dcf
mitanshih <milkteashih@gmail.com>
2024-12-26 00:00:20 +0800

    Time: O(n) Do it again to verify the outcomes. Add lots of comments to explain my thoughts.

--- 57.py
+++ 57.py
@@ -13,35 +13,62 @@ Created on 2024-12-24 16:47:34
 
 
 #%%    Functions
+#https://leetcode.com/problems/insert-interval/solutions/5975742/video-2-solutions-bonus/
+class Solution_reference:
+    def insert(self, intervals: list[list[int]], newInterval: list[int]
+               ) -> list[list[int]]:
+        result: list[list[int]] = []
+
+        for interval in intervals:
+            if newInterval[1] < interval[0]:      #overlapping
+                result.append(newInterval)
+                newInterval = interval    #update `newInterval`
+            elif newInterval[0] > interval[1]:    #non-overlapping
+                result.append(interval)
+            else:                         #adjust `newInterval`
+                newInterval[0] = min(newInterval[0], interval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+
+        result.append(newInterval)        #append the last interval
+        return result
+
+
 class Solution:
     def insert(self, intervals: list[list[int]], newInterval: list[int]
                ) -> list[list[int]]:
-        if intervals == []:    #edge case
-            return [newInterval]
         result: list[list[int]] = []
 
-        for interval in intervals:    #find the place to insert
+        # Insert `newInterval` at the place
+        # where `intervals[index][0]` is not smaller than `newInterval[0]`.
+        for interval in intervals:
             if interval[1] >= newInterval[0]:
                 break
+
             result.append(interval)
 
         current: int = len(result)
-        limit: int = len(intervals)
+        length: int = len(intervals)
 
-        #check overlapping with the front of `newInterval`
-        if current < limit and intervals[current][0] < newInterval[1]:
+        # For the *beginning* of `newInterval`,
+        # use *minimum* value between `intervals[current]` and `newInterval`.
+        if current < length and intervals[current][0] < newInterval[1]:
             newInterval[0] = min(newInterval[0], intervals[current][0])
 
-        while current < limit:    #the range instead by `newInterval`
-            if intervals[current][0] > newInterval[1]:
+        # Updated/Instead the interval while after adding the `newInterval`.
+        while current < length:
+            if newInterval[1] < intervals[current][0]:
                 break
+
             current += 1
 
-        if current == 0:    # `newInterval` is the first interval.
-            result.append(newInterval)
-        else:
-            result.append([newInterval[0],    #the end point of final interval
-                           max(intervals[current - 1][1], newInterval[1])])
+        # When `current` is 0, prepend `newInterval` to `intervals`.
+        # Conversely, for the *end* point of `newInterval`,
+        # use the *maximum* value
+        # between `intervals[current - 1]` and `newInterval`.
+        if current != 0:
+            newInterval[1] = max(newInterval[1], intervals[current - 1][1])
+        result.append(newInterval)    #append `newInterval` into `result`
+
         result.extend(intervals[current:])    #extend the rest of `intervals`
 
         return result
