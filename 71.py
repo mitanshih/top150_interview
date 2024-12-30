e6c60676b317d8a78de5a7100b148cd954b0fd2e
mitanshih <milkteashih@gmail.com>
2024-12-30 22:48:17 +0800

    Time: O(n + n) Make the code readable through match-case implementation, and operate `index` to simplify the canonical path. Replace (remove) a directory by moving `index` backward. If an empty or current directory is found, pass this iteration; otherwise, `path_list[index] = directory` complements the directory and moves `index` forward.

--- 71.py
+++ 71.py
@@ -12,6 +12,26 @@ Created on 2024-12-30 16:17:05
 
 
 #%%    Functions
+class Solution_optimize:
+    def simplifyPath(self, path: str) -> str:
+        path_list: list[str] = path.split('/')    #separate `path` with '/'
+        index: int = 0    # The count for simplified items in list is checked.
+
+        for directory in path_list:
+            match directory:
+                case '' | '.':    #pass empty/current directory
+                    continue
+                case '..':        #remove directory to the parent's
+                    if index > 0:
+                        index -= 1    #move `index` backward to instead
+
+                case _:
+                    path_list[index] = directory    #complement `item`
+                    index += 1    #move `index` forward with simplified `item`
+            #
+        return '/' + '/'.join(path_list[:index])  #simplified items in range
+
+
 class Solution:
     def simplifyPath(self, path: str) -> str:
         simplified_path_list: list[str] = []
